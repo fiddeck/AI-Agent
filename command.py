048940f685a3a300225c2cb1450665e1f5316407
电影剪辑生 <wangying2022421@outlook.com>
2025-10-21 01:59:12 +0800

    优化功能

--- command.py
+++ command.py
@@ -5,4 +5,4 @@ venv_python = os.path.abspath(r".venv\Scripts\python.exe")
 script_path = os.path.abspath("chat.py")
 
 # Windows 下用 start 命令打开新控制台
-subprocess.Popen(f'start cmd /k "{venv_python} {script_path}"', shell=True)
+subprocess.Popen(f'start cmd /k ".venv\\Scripts\\python chat.py"', shell=True)
